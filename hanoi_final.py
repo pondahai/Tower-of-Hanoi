d8b268be8e78920948261b806365c1f6924980dc
pondahai <dahai.pon@gmail.com>
2020-05-26 16:38:57 +0800

    Update hanoi_final.py

--- hanoi_final.py
+++ hanoi_final.py
@@ -1,8 +1,9 @@
 #步驟計數器
 steps=1
 #產生演算法並顯示過程
-def hanoi(n, a, b, c,poles):
+def hanoi(n, a, b, c):
     global steps
+    global poles
     if n == 1:
         print("第%d步： 盤由柱 %c 移至柱 %c" % (steps,a,c))
         steps+=1
@@ -11,17 +12,17 @@ def hanoi(n, a, b, c,poles):
         print()
         
     else:
-        hanoi(n-1, a, c, b,poles)
-        hanoi(1, a, b, c,poles)
-        hanoi(n-1, b, a, c,poles)
+        hanoi(n-1, a, c, b)
+        hanoi(1, a, b, c)
+        hanoi(n-1, b, a, c)
 
 #使用者輸入盤子數目
-n = input("請輸入整數：")
+discs = input("請輸入整數：")
 #製作柱子變數 
-poles=[list(range(1,int(n)+1)),[],[]]
+poles=[list(range(1,int(discs)+1)),[],[]]
 #印出柱子初始狀態
 print(poles)
 print()
 #運算並列舉出演算法結果
-hanoi(int(n), '1', '2', '3',poles)
+hanoi(int(discs), '1', '2', '3')
 print('完成')
